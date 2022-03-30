13e8780a4b70f6b688ea6a00a6ab9fb618b6280b
Armando Gonzalez <snaider1507@tutanota.com>
2022-03-30 09:44:57 -0400

    nur channel added

---
+++ laptop/configuration.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ...}:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      /etc/nixos/common.nix
+    ];
+  
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  networking.useDHCP = false;
+  networking.interfaces.eno1.useDHCP = true;
+  networking.interfaces.wlp4s0.useDHCP = true;
+  networking.hostId = "dfbbc1c6"
+
+}
+
